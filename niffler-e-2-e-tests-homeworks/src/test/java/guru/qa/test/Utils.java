df84aea8fe366dc31a14488c1e2d05dd2a68e693
yarov <yaroslav.vinokurov@gmail.com>
2024-12-16 14:35:35 -0500

    Added Utils and RandomDataUtils

---
+++ niffler-e-2-e-tests-homeworks/src/test/java/guru/qa/test/Utils.java
@@ -0,0 +1,20 @@
+package guru.qa.test;
+
+import com.github.javafaker.Faker;
+
+public class Utils {
+
+  public static String generateRandomString(int length) {
+    if (length <= 0) {
+      throw new IllegalArgumentException("Length must be greater than 0");
+    }
+
+    StringBuilder sb = new StringBuilder(length);
+    Faker faker = new Faker();
+    String randomString = faker.lorem().characters(length - 1);
+    sb.append(Character.toUpperCase(randomString.charAt(0)));
+    sb.append(randomString.substring(1));
+
+    return sb.toString();
+  }
+}
